54a937ef6750d2b55f2dcf3190e062c718e2ab9d
Max Koiwai <maxjacobkoiwai@gmail.com>
2020-10-11 16:13:16 -0400

    x

--- op/op/settings.py
+++ op/op/settings.py
@@ -171,4 +171,4 @@ SIMPLE_JWT = {
 
 django_heroku.settings(locals())
 import dj_database_url
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
