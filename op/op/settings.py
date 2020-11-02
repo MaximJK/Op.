287cc4eed2a04b6d8277aaea0f78e6b4af5354c5
Max Koiwai <maxjacobkoiwai@gmail.com>
2020-11-02 17:37:21 -0500

    x

--- op/op/settings.py
+++ op/op/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/op/static/'
+STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
