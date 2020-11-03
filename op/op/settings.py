6e4a666b0b2543ecfd061b673dc44042d091a392
Max Koiwai <maxjacobkoiwai@gmail.com>
2020-11-03 13:19:02 -0500

    x

--- op/op/settings.py
+++ op/op/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'xxxxxxxxxxxx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["opusv.herokuapp.com"]
 
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
