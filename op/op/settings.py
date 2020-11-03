1a630e7b7b4c0d80993dcb66753ca2c741e9e8ac
Max Koiwai <maxjacobkoiwai@gmail.com>
2020-11-02 20:40:57 -0500

    x

--- op/op/settings.py
+++ op/op/settings.py
@@ -138,10 +138,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = ( 
-     os.path.join(PROJECT_DIR, "static"), 
+     os.path.join(BASE_DIR, "static"), 
 )
 
 # Custom user model
