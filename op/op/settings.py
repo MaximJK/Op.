001ee34d16892feb68f9aef52c788426e82d4334
Max Koiwai <maxjacobkoiwai@gmail.com>
2020-11-02 20:12:08 -0500

    x

--- op/op/settings.py
+++ op/op/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECR_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = ( 
      os.path.join(BASE_DIR, "static"), 
