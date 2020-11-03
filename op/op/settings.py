104f10fbc1c12255bb0b4e7a4cfa00e816225e6e
Max Koiwai <maxjacobkoiwai@gmail.com>
2020-11-02 19:59:46 -0500

    x

--- op/op/settings.py
+++ op/op/settings.py
@@ -137,12 +137,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = ( 
      os.path.join(BASE_DIR, "static"), 
 )
 # Custom user model
+
 AUTH_USER_MODEL = "authentication.CustomUser"
 
 # Rest Framework
