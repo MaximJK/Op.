075a6a5fdccf6a1d92b95cd541420588027ce995
Max Koiwai <maxjacobkoiwai@gmail.com>
2020-11-09 18:49:00 -0500

    x

--- op/frontend/src/actions/axiosApi.js
+++ op/frontend/src/actions/axiosApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-// const baseURL = 'http://127.0.0.1:8000/api/'
+
 const baseURL = 'https://opusv.herokuapp.com/api/'
 const axiosInstance = axios.create({
     baseURL: baseURL,
